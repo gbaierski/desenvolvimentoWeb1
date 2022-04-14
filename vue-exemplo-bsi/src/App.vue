2cee48a7f58e4f2aa4675081dcf1f40ec268c9e4
Gustavo <gustavobaierskiescola@gmail.com>
2022-04-14 09:00:28 -0300

    Atualizar primeiro projeto de vue

--- vue-exemplo-bsi/src/App.vue
+++ vue-exemplo-bsi/src/App.vue
@@ -2,32 +2,122 @@
 export default {
   data() {
     return {
-      nome: "Gustavo Baierski",
-      origem: "Brasileiro",
+      nome: "Gustavo J. Baierski",
+      idade: "20",
+      cpf: "000000000000",
+      nacionalidade: "Brasileiro",
       likes: 0,
+      valor: 6.0,
+      frutas: ["Pera", "Uva", "Laranja", "Pêssego"],
+      novocarro: {
+        id: 3,
+        desc: "Mclaren Senna",
+      },
+      carros: [
+        {
+          id: 0,
+          desc: "Corsa",
+        },
+        {
+          id: 1,
+          desc: "Silvia S15",
+        },
+        {
+          id: 2,
+          desc: "Maverick V8 aspirado 8 marchas turbo",
+        },
+      ],
     };
   },
   methods: {
-    like() {
-      this.likes += 1;
+    addLike() {
+      this.likes = this.likes + 1;
     },
-    deslike() {
-      this.likes -= 1;
+    remLike() {
+      this.likes = this.likes - 1;
+    },
+    getLikes() {
+      return this.likes;
+    },
+    adicionarCarro() {
+      this.carros.push(this.novocarro);
+      this.novocarro = {};
+
+      const pos = this.carros.findIndex((c) => c === this.novocarro);
+      console.log(pos);
+    },
+    removerCarro(carro) {
+      const pos = this.carros.findIndex((c) => c === carro);
+      this.carros.splice(pos, 1);
+    },
+  },
+  computed: {
+    meta1() {
+      return this.likes === 3;
+    },
+    getLikes2() {
+      return this.likes;
+    },
+    icms() {
+      let porcent = 0.2;
+      if (this.valor < 5) porcent = 0.3;
+      else if (this.valor < 6) porcent = 0.25;
+      return this.valor * porcent;
     },
   },
 };
 </script>
+<script setup></script>
 
 <template>
-  <h1>Olá Vue!</h1>
+  <h1>Olá Vue</h1>
   <h2>Nome: {{ nome }}</h2>
-  <h2>Origem: {{ origem }}</h2>
+  <h2>Idade: {{ idade }}</h2>
+  <h2>Cpf: {{ cpf }}</h2>
   <h2>Likes: {{ likes }}</h2>
+  <button v-on:click="addLike">Like</button>
+  <button v-on:click="remLike">Dislike</button>
 
-  <button v-on:click="like()">Like</button>
-  <button v-on:click="deslike()">Deslike</button>
-
-  <input type="text" v-model="nome" />
+  <div v-show="meta1">Parabéns! Você alcançou a meta (v-show)</div>
+  <div v-if="(likes >= 4) & (likes < 6)">
+    Parabéns! Você alcançou a meta {{ getLikes() }} (v-if)
+  </div>
+  <div v-else-if="likes >= 6">
+    Parabéns! Você alcançou a meta secreta, troque o seu nome (v-elseif)
+    <input type="text" v-model="nome" />
+  </div>
+  <div v-else>Número de likes: ({{ getLikes2 }})</div>
+  <hr />
+  <input type="number" v-model="valor" />
+  <p>Valor combustivel:{{ valor }} com ICMS :{{ icms }}</p>
+  <hr />
+  <div>
+    <h1>Frutas</h1>
+    <ul>
+      <li v-for="(fruta, i) in frutas" v-bind:key="i">
+        {{ i }} -- {{ fruta }}
+      </li>
+    </ul>
+  </div>
+  <hr />
+  <div>
+    <h1>Carros</h1>
+    <input
+      type="text"
+      placeholder="ID"
+      :value="novocarro.id"
+      @input="novocarro.id = $event.target.value"
+    />
+    <input type="text" v-model="novocarro.desc" />
+    <button v-on:click="adicionarCarro">Adicionar</button>
+    <div>{{ novocarro.id }} -- {{ novocarro.desc }}</div>
+    <ul>
+      <li v-for="carro in carros" :key="carro.id">
+        <button @click="removerCarro(carro.id)">Remover</button>
+        {{ carro.id }} -- {{ carro.desc }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style>

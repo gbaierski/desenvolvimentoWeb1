20c5bbf158b0d97eb1822f9f0720a22d245755e1
gbaierski <gustavobaierski@gmail.com>
2022-04-28 11:55:23 -0300

    Adicionar titulo e corrigir css

--- kanbanDevWeb/src/App.vue
+++ kanbanDevWeb/src/App.vue
@@ -4,7 +4,11 @@ import CardContainer from './components/CardContainer.vue';
 
 <template>
     <section id="kanbanContainer">
-        <h1>Kanban DevWeb</h1>
+        <h1>
+            <div id="bordaEsq" class="bordaTitulo"></div> 
+            <div id="bordaDir" class="bordaTitulo"></div> 
+            Kanban DevWeb
+        </h1>
         <div id="kanban">
             <CardContainer />
             <CardContainer />
@@ -15,121 +19,148 @@ import CardContainer from './components/CardContainer.vue';
 </template>
 
 <style>
- body {
-      margin: 0;
-      background: linear-gradient(90deg, hsla(334, 34%, 58%, 1) 0%, hsla(191, 63%, 61%, 1) 100%);
-      color: white;
-      font-family: 'Muli',sans-serif;
-  }
+body {
+    margin: 0;
+    background: linear-gradient(90deg, hsla(334, 34%, 58%, 1) 0%, hsla(191, 63%, 61%, 1) 100%);
+    color: white;
+    font-family: 'Muli',sans-serif;
+}
 
-  h1 {
-    font-size: 200%;
+h1 {
+    height: 35px;
+    font-size: 180%;
     font-weight: bold;
     text-align: center;
-  }
+    border-radius: 10px;
+    background-color: rgb(46, 46, 46);
+    padding: 20px 50px 10px 50px;
+    transform: translateY(25px);
+}
 
-  #kanbanContainer {
-      width: 100vw;
-      height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-  }
+.bordaTitulo {
+    width: 80px;
+    height: 50px;
+    position: absolute;
+    background-color:rgb(46, 46, 46);
+    border-radius: 10px 10px 0 0;
+    border-top: solid 1px red;
+}
 
-  #kanban {
-      width: 90%;
-      height: 600px;
-      background-color: rgb(46, 46, 46);
-      display: flex;
-      align-items: center;
-      justify-content: space-around;
-      border-radius: 5px;
-  }
+#bordaEsq {
+    transform: translateX(-100px);
+    padding-right: 20px; 
+    border-left: solid 1px red;
+}
 
-  .cardsContainer {
-      width: 23%;
-      height: 90%;
-      border-radius: 5px;
-      background: transparent;
-      border: solid 1px hsla(334, 34%, 58%, 1);
-      overflow:auto;
-  }
+#bordaDir {
+    transform: translateX(+230px);
+    padding-left: 20px; 
+    border-right: solid 1px red;
+}
 
-  .cardsContainer:nth-child(odd) {
-      border: solid 1px hsla(191, 63%, 61%, 1);
-  }
+#kanbanContainer {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 
-  .tituloCardContainer {
-      font-size: 140%;
-      padding: 2%;
-      text-align: center;
-      height: 5%;
-  }
+#kanban {
+    width: 90%;
+    height: 600px;
+    background-color: rgb(46, 46, 46);
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    border-radius: 5px;
+}
 
-  .cards {
-        width: 100%;
-        height: 93%;
-        border-radius: 5px;
-        display: flex;
-        flex-direction: column;
-        padding-bottom: 5%;
-  }
+.cardsContainer {
+    width: 23%;
+    height: 90%;
+    border-radius: 5px;
+    background: transparent;
+    border: solid 1px hsla(334, 34%, 58%, 1);
+    overflow:auto;
+}
 
-  .card {
-      width: 90%;
-      height: 20%;
-      margin-top: 5%;
-      margin-left: 5%;
-      background-color: white;
-      color: black;
-      text-align: center;
-      border-radius: 5px;
-      padding-top: 10px;
-      padding-bottom: 10px;
-  }
+.cardsContainer:nth-child(odd) {
+    border: solid 1px hsla(191, 63%, 61%, 1);
+}
 
-  .tituloCard {
-      font-weight: bold;
-      font-size: 110%;
-      margin-top: 2%;
-  }
+.tituloCardContainer {
+    font-size: 140%;
+    padding: 2%;
+    text-align: center;
+    height: 5%;
+}
 
-  .descCard {
-      font-size: 80%;
-      margin-top: 5%;
-  }
+.cards {
+    width: 100%;
+    height: 93%;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 5%;
+}
 
-  .tagsCard {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      margin-top: 5%;
-  }
+.card {
+    width: 90%;
+    height: 20%;
+    margin-top: 5%;
+    margin-left: 5%;
+    background-color: white;
+    color: black;
+    text-align: center;
+    border-radius: 5px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
 
-  .tag {
-      font-size: 60%;
-      padding: 3px;
-      color: white;
-      font-weight: bold;
-      margin-left: 2%;
-      width: 25%;
-      border-radius: 5px;
-  }
-  .tag:nth-child(1) {
-      background: hsla(26, 89%, 48%, 1);
-      background: linear-gradient(90deg, rgb(148, 95, 118) 0%, rgb(81, 136, 148) 100%);
-  }
+.tituloCard {
+    font-weight: bold;
+    font-size: 110%;
+    margin-top: 2%;
+}
 
-  .tag:nth-child(2) {
-      background: hsla(136, 100%, 54%, 1);
-      background: linear-gradient(90deg, rgb(96, 158, 112) 0%, rgb(26, 129, 131) 100%);
-  }
+.descCard {
+    font-size: 80%;
+    margin-top: 5%;
+}
 
-  .tag:nth-child(3) {
-      background: hsla(201, 100%, 54%, 1);
-      background: linear-gradient(90deg, rgb(81, 115, 133) 0%, rgb(49, 72, 97) 100%);
-  }
+.tagsCard {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5%;
+}
+
+.tag {
+    font-size: 60%;
+    padding: 3px;
+    color: white;
+    font-weight: bold;
+    margin-left: 2%;
+    width: 25%;
+    border-radius: 5px;
+}
+.tag:nth-child(1) {
+    background: hsla(26, 89%, 48%, 1);
+    background: linear-gradient(90deg, rgb(148, 95, 118) 0%, rgb(81, 136, 148) 100%);
+}
+
+.tag:nth-child(2) {
+    background: hsla(136, 100%, 54%, 1);
+    background: linear-gradient(90deg, rgb(96, 158, 112) 0%, rgb(26, 129, 131) 100%);
+}
+
+.tag:nth-child(3) {
+    background: hsla(201, 100%, 54%, 1);
+    background: linear-gradient(90deg, rgb(81, 115, 133) 0%, rgb(49, 72, 97) 100%);
+}
 
   /* width */
 ::-webkit-scrollbar {
